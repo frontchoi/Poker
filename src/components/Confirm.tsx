2cfa8006849f474dca5291b78e448ab3771e29f1
frontchoi@gmail.com <frontchoi@gmail.com>
2024-09-01 04:56:46 +0900

    feat: 승자 예측 팝업 추가2

--- src/components/Confirm.tsx
+++ src/components/Confirm.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TUserName } from '../types';
 
 const Confirm = () => {
+    const [isSelect, setIsSelect] = useState<TUserName>('');
+
+    const userSelect = (user: TUserName) => {
+        setIsSelect((prevUser) => (prevUser === user ? '' : user));
+    };
+
     return (
         <div className="confirm-popup">
             <div className="popup-content">
                 <p>게임의 승자를 예측해 보세요.</p>
                 <ul>
-                    <li className="photo black"></li>
-                    <li className="photo orange"></li>
-                    <li className="photo red"></li>
-                    <li className="photo yellow"></li>
-                    <li className="photo green"></li>
+                    <li className={`photo black ${isSelect === 'black' ? 'check' : ''}`} onClick={() => userSelect('black')}></li>
+                    <li className={`photo orange ${isSelect === 'orange' ? 'check' : ''}`} onClick={() => userSelect('orange')}></li>
+                    <li className={`photo red ${isSelect === 'red' ? 'check' : ''}`} onClick={() => userSelect('red')}></li>
+                    <li className={`photo yellow ${isSelect === 'yellow' ? 'check' : ''}`} onClick={() => userSelect('yellow')}></li>
+                    <li className={`photo green ${isSelect === 'green' ? 'check' : ''}`} onClick={() => userSelect('green')}></li>
                 </ul>
-                <button>확인</button>
+                <button disabled={isSelect === '' ? true : false}>확인</button>
             </div>
         </div>
     );
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 export type TCardShape = 'diamond' | 'club' | 'heart' | 'spade';
-export type TUserName = 'green' | 'black' | 'orange' | 'red' | 'yellow';
+export type TUserName = '' | 'green' | 'black' | 'orange' | 'red' | 'yellow';
 
 export interface IPlayerProps {
     photo: TUserName;
