39f745a70d81f058397e2bf737c863eab70baca6
frontchoi@gmail.com <frontchoi@gmail.com>
2024-09-01 00:20:51 +0900

    fix: save

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { TUserPhoto, IUserProps } from "../types";
+import Cards from "./Cards";
 
 const Player: React.FC<IUserProps> = ({ photo }) => {
   return (
     <div className="card-box">
       <div className={`photo ${photo}`}></div>
-      <div className="cards">adf</div>
+      <div className="cards">
+        <Cards shape="club" num={1} />
+      </div>
     </div>
   );
 };
